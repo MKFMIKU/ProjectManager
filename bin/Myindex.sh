1f3aca1b4583016a662f9d871eea1cdbd27dc7f3
MKFMIKU <mikumkf@outlook.com>
2016-07-02 00:07:08 +0800

    Change sh

--- bin/Myindex.sh
+++ bin/Myindex.sh
@@ -3,8 +3,6 @@ PROJECT_PATH = '/home/wwwroot/Myindex'
 echo "--Auto Deploy--"
 cd $PROJECT_PATH
 echo "pulling source code..."
-git reset --hard origin/master
-git clean -f
 git pull
 git checkout master
 pm2 restart 0
