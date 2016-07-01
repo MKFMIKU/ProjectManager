c912b17e0c56b203e55badc7af8273bc369e410d
MKFMIKU <mikumkf@outlook.com>
2016-07-01 23:55:37 +0800

    Add

---
+++ README.md
@@ -0,0 +1,1 @@
+// This is My Manager to Git//
---
+++ index.js
@@ -0,0 +1,31 @@
+var http = require('http'),
+	createHandler = require('github-webhook-handler'),
+	indexHandler = createHandler({ path: '/Myindex', secret: '12345678' });
+
+function run_cmd(cmd, args, callback) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var resp = "";
+ 
+  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
+  child.stdout.on('end', function() { callback (resp) });
+}
+
+
+http.createServer(function (req, res) {
+  indexHandler(req, res, function (err) {
+    res.statusCode = 404;
+    res.end('Api for Myindex');
+  });
+}).listen(4000);
+
+indexHandler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+indexHandler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref);
+  run_cmd('sh', ['./bin/Myindex.sh'], function(text){ console.log(text) });
+})
