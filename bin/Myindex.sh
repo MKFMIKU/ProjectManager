b28c052c859c76e111a57144482b61b70162c35a
KF M <KF M>
2016-07-08 13:02:00 +0800

    Change sh

--- bin/Myindex.sh
+++ bin/Myindex.sh
@@ -1,11 +1,9 @@
 PROJECT_PATH="/home/wwwroot/Myindex"
 PROHECT_ID="www"
 
-echo "--Auto Deploy--"
+echo "--Auto Deploy for Myindex--"
 cd $PROJECT_PATH
 echo "pulling source code..."
-pm2 stop $PROHECT_ID
 git pull
-git checkout master
 pm2 restart $PROHECT_ID
 echo "Finished."
--- bin/lab.sh
+++ bin/lab.sh
@@ -1,11 +1,9 @@
 PROJECT_PATH="/home/DormitoryD"
 PROHECT_ID="index"
 
-echo "--Auto Deploy--"
+echo "--Auto Deploy for lab--"
 cd $PROJECT_PATH
 echo "pulling source code..."
-pm2 stop $PROHECT_ID
 git pull
-git checkout master
 pm2 restart $PROHECT_ID
 echo "Finished."
